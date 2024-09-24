eca44638ad1e154d76e26013c327a3d61544ed88
Sabridh <sabri.dh@icloud.com>
2024-09-24 11:16:03 +0200

    Update begin.mjs

---
+++ begin.mjs
@@ -0,0 +1,13 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+let name = await userInput.question('Wat is je naam? ');
+let age = await userInput.question('hoe oud en je? ');
+let favoriteGame = await userInput.question('Wat is je lievelingsgame? ');
+
+
+
+
+
+console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
